59af98e1c23f4dccc8826c8f397d6e8675722268
Bojan Mitrovic <Kuca@bojans-mac-mini.attlocal.net>
2019-01-24 14:36:00 -0800

    initial commit

---
+++ javascript.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBrSTPsvdrOcqfCN00J-7NvMgWX0u9oyy8",
+    authDomain: "fir-103ef.firebaseapp.com",
+    databaseURL: "https://fir-103ef.firebaseio.com",
+    projectId: "fir-103ef",
+    storageBucket: "fir-103ef.appspot.com",
+    messagingSenderId: "585461704571"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+  
